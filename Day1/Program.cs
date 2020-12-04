d0713ea58816c2b0a065933a13fa77708c338034
Hubert Rybak <HRY@compugame.dk>
2020-12-04 09:52:49 +0100

    Day 1

---
+++ Day1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+// 2020 = x + y
+// y = x * y
+
+var expectedSum = 2020;
+
+var entries = File.ReadAllLines("input.txt").Select(int.Parse).ToHashSet();
+
+foreach (var foundX in entries)
+{
+    var y = expectedSum - foundX;
+    if (entries.TryGetValue(y, out var foundY))
+    {
+        Console.WriteLine($"Result: {foundX * foundY}");
+        return;
+    }
+}
